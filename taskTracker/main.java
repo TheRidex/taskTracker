826cf643bd0f22d54c5949215ef89c79f8a6f1a0
TheRidex <mathiaslevi6@gmail.com>
2026-06-04 16:57:10 +0100

    Basic options menu addedd to the Main class

--- taskTracker/main.java
+++ taskTracker/main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("hey user, what would you like to do today?\n" +
+                "Option 1: create a new entry\n" +
+                "Option 2: select a previous entry\n" +
+                "Option 3: exit the program");
+
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                System.out.println("this is case 1");
+                break;
+
+            default:
+                System.out.println("invalid input");
+                break;
+        }
 
     }
 }
