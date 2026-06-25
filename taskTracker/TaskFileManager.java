4fabf4cb7bd854e580368cb4f9b52cfc3200d994
TheRidex <mathiaslevi6@gmail.com>
2026-06-25 11:43:48 +0100

    Addedd a method in TaskFileManager that lets the program write to a file, next I need to implement reading from file

--- taskTracker/TaskFileManager.java
+++ taskTracker/TaskFileManager.java
@@ -1,16 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class TaskFileManager {
 
     // save tasks
     public void saveTasks(ArrayList<Task> tasks) {
-        for (Task item : tasks) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("Output.txt"));
+            for (Task item : tasks) {
+                writer.write(item.getId() + "|" +
+                        item.getDescription() + "|" +
+                        item.getComplete() + "\n");
 
-            System.out.println(item.getId() + "|" + item.getDescription() + "|" + item.getComplete());
-
+            }
+            writer.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
-
 }
 
 // Load tasks
